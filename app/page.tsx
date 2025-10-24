e586ab748bda574c4a80e8b8be4dd7c9428b7c9b
Karenartc <karenrc.rrss@gmail.com>
2025-10-23 22:02:32 -0300

    refactor: Elimina componente dev ThemeToggle

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ import HeroSection from '@/components/sections/HeroSection';
 import FeaturesSection from '@/components/sections/FeaturesSection';
 import DownloadSection from '@/components/sections/DownloadSection';
 import Footer from '@/components/commons/Footer';
-import ThemeToggle from '@/components/dev/ThemeToggle';
 
 export default function PublicHomePage() {
   return (
@@ -16,7 +15,6 @@ export default function PublicHomePage() {
       <FeaturesSection />
       <DownloadSection />
       <Footer />
-      <ThemeToggle /> {/* Botón temporal */}
     </>
   );
 }
