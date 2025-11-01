1a1f98adb56ebc0e63cba601148705e28e314787
Karenartc <karenrc.rrss@gmail.com>
2025-11-01 13:59:24 -0300

    Corrige configuración PWA y acceso a manifest en deploy

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,15 @@ const withPWA = require('next-pwa')({
   dest: 'public',       
   register: true,     
   skipWaiting: true,  
-  disable: process.env.NODE_ENV === 'development', 
+  disable: process.env.NODE_ENV === 'development',
+  scope: '/',
+  sw: 'sw.js', // 
+  buildExcludes: [/middleware-manifest\.json$/], 
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
 };
 
 module.exports = withPWA(nextConfig);
